8e6ae1da1b514f4066b86ba0e047275a0d9dfbce
Sarvesh Singh <sarvesh.singh@lseg.com>
2022-09-07 08:16:44 +0530

    Add project files.

---
+++ WebApplication2/LanguagePref.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class LanguagePref
+    {
+        public LanguagePref()
+        {
+
+        }
+
+        public string My_pref()
+        {
+            return "Hindi";
+        }
+    }
+}
